633525449988c4189d58c18c7ff651c3f08cc664
Алексей Дубовой <aleksejdubovoj@Aleksejs-MacBook-Pro.local>
2023-03-29 20:25:52 +0200

    remove throttle function

--- src/utils.ts
+++ src/utils.ts
@@ -5,18 +5,3 @@ export function pause(time: number = 1000) {
 export function removeDuplicates<T>(array: T[]): T[] {
   return Array.from(new Set(array));
 }
-
-export function throttle<T extends (...args: any[]) => any>(
-  func: T,
-  wait: number
-): T {
-  let timeout: number | undefined;
-  return ((...args: any[]) => {
-    if (!timeout) {
-      timeout = window.setTimeout(() => {
-        timeout = undefined;
-        func(...args);
-      }, wait);
-    }
-  }) as T;
-}
